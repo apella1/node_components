f3d24e3816ef55a83a0ee2fc0756ff614b0b1ccd
apella1 <johncollons5@gmail.com>
2023-03-29 16:17:04 +0300

    Configure TypeScript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import os from "os";
+import path from "path";
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+console.log(global);
+
+console.log(__dirname);
+console.log(__filename);
